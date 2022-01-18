a565e55fed9065bb5ed639bdf3809aa1bb711d87
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-01-18 20:05:20 +0100

    phone number field added

--- frontend-bankMaterielsManagement/src/app/app.module.ts
+++ frontend-bankMaterielsManagement/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,12 +7,15 @@ import { HomeComponent } from './home/home.component';
 import { AgencyDashboardComponent } from './agency-dashboard/agency-dashboard.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginUerComponent,
     HomeComponent,
+    HeaderComponent,
     AgencyDashboardComponent
   ],
   imports: [
