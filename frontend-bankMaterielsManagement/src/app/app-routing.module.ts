ff7da303fecea93eb576ff5962a639c98642b5a5
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-01-18 21:26:32 +0100

    equipment container created

--- frontend-bankMaterielsManagement/src/app/app-routing.module.ts
+++ frontend-bankMaterielsManagement/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgencyDashboardComponent } from './agency-dashboard/agency-dashboard.component';
+import { EquipementComponentComponent } from './equipement-component/equipement-component.component';
 import { HomeComponent } from './home/home.component';
 import { LoginUerComponent } from './login-uer/login-uer.component';
 
@@ -8,7 +9,8 @@ const routes: Routes = [
   {path:'', component: LoginUerComponent},
  { path:'loginUser', component: LoginUerComponent},
  {path: 'home' , component: HomeComponent},
- {path: 'agencies', component: AgencyDashboardComponent}
+ {path: 'agencies', component: AgencyDashboardComponent},
+ {path: 'equipement', component: EquipementComponentComponent} 
 ];
 
 @NgModule({
