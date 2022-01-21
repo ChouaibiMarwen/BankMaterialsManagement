e89d55789592ad67db91b2307174b64b90cf46d6
iheb dhouibi <ihebdhouibi@polytechnicien.tn>
2022-01-21 19:03:38 +0100

    AddEquipment modal showing

--- frontend-bankMaterielsManagement/src/app/equipement-component/equipement-component.component.ts
+++ frontend-bankMaterielsManagement/src/app/equipement-component/equipement-component.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ViewChild } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-equipement-component',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipementComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal,) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+   }
 
   ngOnInit(): void {
   }
 
+  open(addEquipmenrt:any) {
+    this.modalService.open(addEquipmenrt);
+  }
 }
